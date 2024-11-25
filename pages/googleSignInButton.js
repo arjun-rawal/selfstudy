25c6aced318dc89a42a000a6bdab377ce3b80b9f
arjun-rawal <arawalarjun@gmail.com>
2024-11-25 01:51:09 -0500

    ads

--- pages/googleSignInButton.js
+++ pages/googleSignInButton.js
@@ -25,10 +25,11 @@ const GoogleSignInButton = () => {
         className="g_id_signin" 
         data-type="standard"
         data-size="large"
-        data-theme="outline"
         data-text="sign_in_with"
         data-shape="rectangular"
         data-logo_alignment="left"  
+        data-theme = "filled_black"
+        
       ></div>
     </div>
   );
