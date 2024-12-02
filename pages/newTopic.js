ebfed97c1dc20da17f551e445013f1774e9b8461
arjun-rawal <arawalarjun@gmail.com>
2024-12-02 09:58:44 -0500

    ad wall

--- pages/newTopic.js
+++ pages/newTopic.js
@@ -15,13 +15,16 @@ import { useState } from "react";
 export default function NewTopic(props) {
   const [timeVal, setTimeVal] = useState("Months");
   const [numVal, setNumVal] = useState(3);
-  const handleNumChange = (newVal) => {
-    setNumVal(newVal.valueAsNumber)
-
-  };
   const [topic, setTopic] = useState("");
-  const username = props.username?.username || ""; 
+  const [showAd, setShowAd] = useState(false);
+  const username = props.username?.username || "";
   const password = props.username?.password || "";
+  const vastUrl = "https://s.magsrv.com/splash.php?idzone=5486412"; // Replace with your VAST URL
+
+  const handleNumChange = (newVal) => {
+    setNumVal(newVal.valueAsNumber);
+  };
+
   async function handleNew() {
     const res = await fetch("/api/submitPlan", {
       method: "POST",
@@ -40,60 +43,85 @@ export default function NewTopic(props) {
     }
   }
 
+  const handleGenerate = () => {
+    setShowAd(true); // Display the video ad
+  };
+
+  const handleAdComplete = () => {
+    setShowAd(false); // Hide the ad
+    handleNew(); // Run the API call after the ad is complete
+  };
+
   return (
-    <>
-      <Center height={"95vh"}>
-        <Stack gap="2vh" width="auto">
-          <Input
-            placeholder="I want to learn..."
-            onChange={(e) => {
-              setTopic(e.target.value);
-            }}
-          />
-          <Stack
-            direction={{ base: "column", md: "row" }}
-            margin={"auto"}
-            alignItems={"center"}
-          >
-            <Text whiteSpace={"no-wrap"}> I have...</Text>
-            <StepperInput value= {numVal} min={1} max={5} onValueChange={handleNumChange} />
-            <MenuRoot positioning={{ placement: "bottom-end" }}>
-              <MenuTrigger asChild>
-                <Button variant="outline" size="sm">
-                  {timeVal}
-                </Button>
-              </MenuTrigger>
-              <MenuContent>
-                <MenuItem
-                  value="Days"
-                  onClick={(e) => {
-                    setTimeVal(e.target.id);
-                  }}
-                >
-                  Days
-                </MenuItem>
-                <MenuItem
-                  value="Weeks"
-                  onClick={(e) => {
-                    setTimeVal(e.target.id);
-                  }}
-                >
-                  Weeks
-                </MenuItem>
-                <MenuItem
-                  value="Months"
-                  onClick={(e) => {
-                    setTimeVal(e.target.id);
-                  }}
-                >
-                  Months
-                </MenuItem>
-              </MenuContent>
-            </MenuRoot>
-          </Stack>
-          <Button onClick = {handleNew}>Generate</Button>
-        </Stack>
-      </Center>
-    </>
+    <Center height={"95vh"}>
+      <Stack gap="2vh" width="auto">
+        {showAd ? (
+          <div>
+            <Text>Watch this ad to proceed:</Text>
+            <video
+              id="video-player"
+              controls
+              width="600"
+              onEnded={handleAdComplete}
+              autoPlay
+            >
+              <source src={vastUrl} type="application/xml+vast" />
+              Your browser does not support the video tag.
+            </video>
+          </div>
+        ) : (
+          <>
+            <Input
+              placeholder="I want to learn..."
+              onChange={(e) => {
+                setTopic(e.target.value);
+              }}
+            />
+            <Stack
+              direction={{ base: "column", md: "row" }}
+              margin={"auto"}
+              alignItems={"center"}
+            >
+              <Text whiteSpace={"no-wrap"}> I have...</Text>
+              <StepperInput value={numVal} min={1} max={5} onValueChange={handleNumChange} />
+              <MenuRoot positioning={{ placement: "bottom-end" }}>
+                <MenuTrigger asChild>
+                  <Button variant="outline" size="sm">
+                    {timeVal}
+                  </Button>
+                </MenuTrigger>
+                <MenuContent>
+                  <MenuItem
+                    value="Days"
+                    onClick={(e) => {
+                      setTimeVal(e.target.id);
+                    }}
+                  >
+                    Days
+                  </MenuItem>
+                  <MenuItem
+                    value="Weeks"
+                    onClick={(e) => {
+                      setTimeVal(e.target.id);
+                    }}
+                  >
+                    Weeks
+                  </MenuItem>
+                  <MenuItem
+                    value="Months"
+                    onClick={(e) => {
+                      setTimeVal(e.target.id);
+                    }}
+                  >
+                    Months
+                  </MenuItem>
+                </MenuContent>
+              </MenuRoot>
+            </Stack>
+            <Button onClick={handleGenerate}>Generate</Button>
+          </>
+        )}
+      </Stack>
+    </Center>
   );
 }
